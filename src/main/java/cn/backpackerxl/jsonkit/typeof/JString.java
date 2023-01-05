06a6c36044230d0bd3ee128d57f985e5a214bdff
千道流 <946115360@qq.com>
2023-01-05 13:38:33 +0800

    ⚙:2023/1/5 init project

---
+++ src/main/java/cn/backpackerxl/jsonkit/typeof/JString.java
@@ -0,0 +1,10 @@
+package cn.backpackerxl.jsonkit.typeof;
+
+/**
+ * @author: backpackerxl
+ * @date: 2023/1/5
+ * @filename: JString
+ * @email: backpackerxh@163.com
+ **/
+public class JString {
+}
